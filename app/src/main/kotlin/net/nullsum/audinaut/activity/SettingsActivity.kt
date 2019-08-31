a5a5a3c6c86c6c4c5799547280dc4866171cd010
Andrew Rabert <ar@nullsum.net>
2019-08-31 19:21:27 -0400

    git-auto-update 2019-08-31T23:21:27+00:00

--- app/src/main/kotlin/net/nullsum/audinaut/activity/SettingsActivity.kt
+++ app/src/main/kotlin/net/nullsum/audinaut/activity/SettingsActivity.kt
@@ -6,7 +6,6 @@ import android.text.InputType
 import androidx.preference.Preference
 import androidx.preference.EditTextPreference;
 import androidx.preference.PreferenceFragmentCompat
-import androidx.preference.PreferenceCategory
 import androidx.preference.PreferenceScreen
 import android.content.SharedPreferences
 import net.nullsum.audinaut.R
@@ -40,7 +39,7 @@ class SettingsActivity : SubsonicActivity() {
                     var fragmentManager = getFragmentManager()
                     if (fragmentManager != null) {
                         fragmentManager.beginTransaction()
-                                .replace(R.id.fragment_container, ServerSettingsFragment())
+                                .replace(R.id.fragment_container, ServerListFragment())
                                 .addToBackStack(null)
                                 .commit()
                         return true
@@ -68,32 +67,41 @@ class SettingsActivity : SubsonicActivity() {
         }
     }
 
-    class ServerSettingsFragment: PreferenceFragmentCompat() {
+    class ServerListFragment: PreferenceFragmentCompat() {
 
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-            val preferenceCategory = findPreference(Constants.PREFERENCES_KEY_SERVER_KEY)
-            val activity = getActivity()
-            val preferences = Util.getPreferences(activity)
+            addPreferencesFromResource(R.xml.settings_servers)
+
+            val context = preferenceManager.context
+            val preferences = Util.getPreferences(context)
+            val serverCount = preferences.getInt(Constants.PREFERENCES_KEY_SERVER_COUNT, 1)
+
+            for (i in 1..serverCount) {
+                Log.d("fuck", "$i")
+            }
+        }
+
+    }
+
+    class ServerInstanceFragment(val instance: Int): PreferenceFragmentCompat() {
+
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            val context = preferenceManager.context
+            val preferences = Util.getPreferences(context)
             val serverCount = preferences.getInt(Constants.PREFERENCES_KEY_SERVER_COUNT, 1)
 
             for (i in 1..serverCount) {
                 Log.d("fuck", "$i")
             }
 
-            setPreferenceScreen(expandServerScreen(1))
-        }
-
-        fun expandServerScreen(id: Int): PreferenceScreen {
-            val activity = getActivity()
-
-            val serverNamePreference = EditTextPreference(activity)
-            serverNamePreference.setKey(Constants.PREFERENCES_KEY_SERVER_NAME + id)
+            val serverNamePreference = EditTextPreference(context)
+            serverNamePreference.key = Constants.PREFERENCES_KEY_SERVER_NAME + instance
             serverNamePreference.setDefaultValue(getResources().getString(R.string.settings_server_unused))
             serverNamePreference.setTitle(R.string.settings_server_name)
             serverNamePreference.setDialogTitle(R.string.settings_server_name)
 
-            val serverUrlPreference = EditTextPreference(activity)
-            serverUrlPreference.setKey(Constants.PREFERENCES_KEY_SERVER_URL + id)
+            val serverUrlPreference = EditTextPreference(context)
+            serverUrlPreference.key = Constants.PREFERENCES_KEY_SERVER_URL + instance
             //serverUrlPreference.getEditText().setInputType(InputType.TYPE_TEXT_VARIATION_URI)
             serverUrlPreference.setDefaultValue("http://yourhost")
             serverUrlPreference.setTitle(R.string.settings_server_address)
@@ -102,47 +110,41 @@ class SettingsActivity : SubsonicActivity() {
             if (serverUrlPreference.getText() == null) {
                 serverUrlPreference.setText("http://yourhost")
             }
-
             serverUrlPreference.setSummary(serverUrlPreference.getText())
 
-            val screen = getPreferenceManager().createPreferenceScreen(activity)
+            val serverLocalNetworkSSIDPreference = EditTextPreference(context)
+            serverLocalNetworkSSIDPreference.key = Constants.PREFERENCES_KEY_SERVER_LOCAL_NETWORK_SSID + instance
+            serverLocalNetworkSSIDPreference.setDefaultValue(getResources().getString(R.string.settings_server_unused))
+            serverLocalNetworkSSIDPreference.setTitle(R.string.settings_server_local_network_ssid)
+            serverLocalNetworkSSIDPreference.setDialogTitle(R.string.settings_server_local_network_ssid)
+
+            val serverInternalUrlPreference: EditTextPreference = EditTextPreference(context)
+            serverInternalUrlPreference.key = Constants.PREFERENCES_KEY_SERVER_INTERNAL_URL + instance
+            serverInternalUrlPreference.setDefaultValue("")
+            serverInternalUrlPreference.setTitle(R.string.settings_server_internal_address)
+            serverInternalUrlPreference.setDialogTitle(R.string.settings_server_internal_address)
+            serverInternalUrlPreference.setSummary(serverInternalUrlPreference.getText())
+
+            val serverUsernamePreference: EditTextPreference = EditTextPreference(context)
+            serverUsernamePreference.key = Constants.PREFERENCES_KEY_USERNAME + instance
+            serverUsernamePreference.setTitle(R.string.settings_server_username)
+            serverUsernamePreference.setDialogTitle(R.string.settings_server_username)
+
+            val serverPasswordPreference: EditTextPreference = EditTextPreference(context)
+            serverPasswordPreference.key = Constants.PREFERENCES_KEY_PASSWORD + instance
+            serverPasswordPreference.setSummary("***");
+            serverPasswordPreference.setTitle(R.string.settings_server_password);
+
+            val screen = getPreferenceManager().createPreferenceScreen(context)
             screen.setTitle(R.string.settings_server_unused);
-            screen.setKey(Constants.PREFERENCES_KEY_SERVER_KEY + id);
+            screen.setKey(Constants.PREFERENCES_KEY_SERVER_KEY + instance);
             screen.addPreference(serverNamePreference)
             screen.addPreference(serverUrlPreference)
-            return screen
+            screen.addPreference(serverLocalNetworkSSIDPreference)
+            screen.addPreference(serverInternalUrlPreference)
+            screen.addPreference(serverUsernamePreference)
+            screen.addPreference(serverPasswordPreference)
+            preferenceScreen = screen
         }
-//            preferenceCategory.addPreference(serverNamePreference)
-            /*
-
-            if (serverNamePreference.getText() == null) {
-                serverNamePreference.setText(getResources().getString(R.string.settings_server_unused));
-            }
-
-        serverNamePreference.setSummary(serverNamePreference.getText());
-            */
-            //addPreferencesFromResource(R.xml.settings_servers)
-
-        /*
-        private fun addServer: PreferenceScreen(instance: Int) {
-            final PreferenceScreen screen = this.getPreferenceManager().createPreferenceScreen(context);
-            screen.setKey(Constants.PREFERENCES_KEY_SERVER_KEY + instance);
-            screen.setOrder(instance);
-
-            screen.setOnPreferenceClickListener(preference -> {
-                SettingsFragment newFragment = new SettingsFragment();
-
-                Bundle args = new Bundle();
-                args.putInt(Constants.PREFERENCES_KEY_SERVER_INSTANCE, instance);
-                newFragment.setArguments(args);
-
-                replaceFragment(newFragment);
-                return false;
-            });
-
-            return screen;
-        }
-        */
-
     }
 }
